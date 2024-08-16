d4b8bc147b0ef9420be2a4372870e0a611478968
Rajesh J <rajeshjames1015@gmail.com>
2024-08-16 16:16:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TictacToe from './Components/TicTacToe/TictacToe';
+
+function App() {
+  return (
+    <div >
+<TictacToe/>
+    </div>
+  );
+}
+
+export default App;
